9a3d7370569dd00a2dac4ea9b639fb78791c9cd7
SLamont <stlamont3134@eagle.fgcu..edu>
2019-08-19 18:42:49 -0400

    attempt1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+//
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
